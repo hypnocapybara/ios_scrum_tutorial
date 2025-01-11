02e755b1ed0895e565fe464951b5db717ac1caaa
Alex Polev <apolevki09@gmail.com>
2025-01-10 21:35:51 -0500

    navigation

--- Scrum/ScrumsView.swift
+++ Scrum/ScrumsView.swift
@@ -4,9 +4,19 @@ struct ScrumsView: View {
     let scrums: [DailyScrum]
 
     var body: some View {
-        List(scrums) { scrum in
-            CardView(scrum: scrum)
+        NavigationStack {
+            List(scrums) { scrum in
+                NavigationLink(destination: Text(scrum.title)) {
+                    CardView(scrum: scrum)
+                }
                 .listRowBackground(scrum.color)
+            }
+            .navigationTitle("Daily Scrums")
+            .toolbar {
+                Button(action: {}) {
+                    Image(systemName: "plus")
+                }
+            }
         }
     }
 }
